53694c739f85a273969cd9bbcc7a3a2df349c763
tyee <tyee@arizona.edu>
2024-11-15 18:06:17 -0700

    More typos fixed. -TY.

--- automotive/bitcount/run-all.sh
+++ automotive/bitcount/run-all.sh
@@ -6,8 +6,8 @@ make
 timestamp=$(date +%Y_%m_%d_%H%M%S)
 
 if [ "$MIBENCH_FAST" = true ] ; then
-    ${MIBENCH_RUN} bitcnts 75000 > $RTLCONFIG_NAME'_'$timestamp'_bitcnts_output_small_.txt'
+    ${MIBENCH_RUN} bitcnts 75000 > $RTLCONFIG_NAME'_'$timestamp'_bitcnts_output_small.txt'
 
 else
-    ${MIBENCH_RUN} bitcnts 1125000 > $RTLCONFIG_NAME'_'$timestamp'_bitcnts_output_large_.txt'
+    ${MIBENCH_RUN} bitcnts 1125000 > $RTLCONFIG_NAME'_'$timestamp'_bitcnts_output_large.txt'
 fi
--- automotive/qsort/run-all.sh
+++ automotive/qsort/run-all.sh
@@ -6,8 +6,8 @@ make
 timestamp=$(date +%Y_%m_%d_%H%M%S)
 
 if [ "$MIBENCH_FAST" = true ] ; then
-    ${MIBENCH_RUN} ./qsort_small input_small.dat > $RTLCONFIG_NAME'_'$timestamp'_qsort_output_small_.txt'
+    ${MIBENCH_RUN} ./qsort_small input_small.dat > $RTLCONFIG_NAME'_'$timestamp'_qsort_output_small.txt'
 
 else
-    ${MIBENCH_RUN} ./qsort_large input_large.dat > $RTLCONFIG_NAME'_'$timestamp'_qsort_output_large_.txt'
+    ${MIBENCH_RUN} ./qsort_large input_large.dat > $RTLCONFIG_NAME'_'$timestamp'_qsort_output_large.txt'
 fi
